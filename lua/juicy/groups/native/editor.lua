42a379e56bd838fa181fe1c187d9c4b2485a8060
cuattoo <timomix13@gmail.com>
2024-07-21 19:03:51 +0200

    Some updates

--- lua/juicy/groups/native/editor.lua
+++ lua/juicy/groups/native/editor.lua
@@ -26,8 +26,8 @@ local groups = {
 
     ErrorMsg = { fg = C.error },                                         -- error messages on the command line
 
-    VertSplit = { fg = C.bg_dark },                                    -- the column separating vertically split windows
-    WinSeparator = { fg = C.bg_dark, bg = C.bg_dark },               -- the column separating vertically split windows
+    VertSplit = { fg = C.green.base, bg = C.bg},                                    -- the column separating vertically split windows
+    WinSeparator = { fg = C.green.base, bg = C.bg },               -- the column separating vertically split windows
 
     Folded = { fg = C.fg_fold, bg = C.bg_fold },                         -- line used for closed folds
     FoldColumn = { bg = C.bg_fold, fg = C.fg_fold },                     -- 'foldcolumn'
